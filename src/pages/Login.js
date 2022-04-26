84300903f67ab356d1fb20a741d7a7b9134f9ca4
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-04-26 16:42:06 -0300

    Requisitos 2 a 6 feitos, falta testar e fazer os testes

---
+++ src/pages/Login.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailInput: '',
+      passwordInput: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  // handleClick = (event) => {
+  //   event.preventDefault();
+  //   const { emailInput } = this.state;
+
+  // }
+
+  validateForm = () => {
+    const { emailInput, passwordInput } = this.state;
+    const numberMin = 6;
+    const passwordValidation = passwordInput.length >= numberMin;
+    const emailValidation = emailInput.match(/\S+@\S+\.\S+/);
+    if (emailValidation && passwordValidation) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isButtonDisabled: true,
+      });
+    }
+    localStorage.setItem('mealsToken', JSON.stringify(1));
+    localStorage.setItem('cocktailsToken', JSON.stringify(1));
+    localStorage.setItem('user', JSON.stringify({ emailInput }));
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, this.validateForm);
+  }
+
+  render() {
+    const { emailInput, passwordInput, isButtonDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="emailInput">
+          <input
+            id="emailInput"
+            name="emailInput"
+            type="text"
+            value={ emailInput }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          <input
+            id="passwordInput"
+            type="text"
+            name="passwordInput"
+            value={ passwordInput }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+        </label>
+        <Link to="/foods">
+          <button
+            type="button"
+            disabled={ isButtonDisabled }
+            data-testid="login-submit-btn"
+          >
+            Enter
+          </button>
+        </Link>
+      </form>
+    );
+  }
+}
+
+export default Login;
