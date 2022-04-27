811dfe68f8b2dfb6e78b01ae7103398bf2f920c2
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-04-27 14:47:34 -0300

    Feita a rota

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Food from './pages/Food';
+import Drink from './pages/Drink';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/foods" component={ Food } />
+      <Route path="/drinks" component={ Drink } />
+      <Route path="/explore" component={ Explore } />
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
     </Switch>
   );
 }
