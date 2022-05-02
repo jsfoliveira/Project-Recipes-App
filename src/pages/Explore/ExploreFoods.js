7e7278e14f6e5343f22c5ae9b29d4ed697edd3f0
Seu nome <halisterfernando@hotmail.com>
2022-05-02 11:14:47 -0300

    requisitos 70 ao 74 finzalizados com rotas dinâmicas mocadas

--- src/pages/Explore/ExploreFoods.js
+++ src/pages/Explore/ExploreFoods.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import './styles/ExploreFoods.css';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
-    <div>
+    <main>
       <Header />
+      <div className="explore-food-container">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explore/foods/ingredients') }
+        >
+          By Ingredient
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+          onClick={ () => history.push('/explore/foods/nationalities') }
+        >
+          By Nationality
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => history.push('/foods/52771') } // alterar para rota dinâmica
+        >
+          Surprise me!
+        </button>
+      </div>
       <Footer />
-    </div>
+    </main>
   );
 }
 
